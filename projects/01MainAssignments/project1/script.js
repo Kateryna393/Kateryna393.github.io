264e42f004ee5ea145cded54998d6c20aa92548e
Kateryna393 <kateryna.nizhnyk@zhdk.ch>
2024-11-28 15:26:21 +0100

    Update script.js

--- projects/01MainAssignments/project1/script.js
+++ projects/01MainAssignments/project1/script.js
@@ -1,29 +1,42 @@
-let textX = 'INTERACTION  DESIGN';
-let fontSize = 45;
-let speed = 0.05;
-let textAmplitude = 20;
-
-
-
-function setup () {
+let textX = 'INTERACTION';
+let fontSize = 20;
+let speed = 0.02;
+let textAmplitude = 50;
+let waveStretch = 0.7;
+let spacing = 20;
+ 
+function setup () {  
   createCanvas(600, 600); 
-  textFont('Georgia'); 
+  myFont = loadFont("venus rising rg.otf"); 
   textSize(fontSize); 
+	textFont(myFont);
   textAlign(CENTER, CENTER);
-  
 }
 
 function draw() {
-  background(0); 
-  fill( random(255), random(255), random(255) ); 
-  
-  let startX = width / 2 - textWidth(textX) / 2; 
-  
-  for (let i = 0; i < textX.length; i++) {
-    let charX = startX + textWidth(textX.substring(0, i)) + textWidth(textX[i]) / 2; 
-    
-    let charY = height / 2 + sin((frameCount * speed) + i) * textAmplitude; 
+	background(0, 10);
 
-    text(textX[i], charX, charY); 
+  let startX = width / 4 - textWidth(textX) / 4;
+	let totalWidth = 0;
+	
+	//colors
+	for (let i = 0; i < textX.length; i++) {
+		  let colorShift = frameCount * 0.01 + i;
+	    let r = map(sin(frameCount * 0.01 +  i), -1, 1, 100, 255);
+      let g = map(sin(frameCount * 0.015 +i), -1, 1, 100, 255);
+      let b = map(sin(frameCount * 0.02 + i), -1, 1, 100, 255);
+      fill(r, g, b);
+  
+	//calculate the position
+  let charX = startX + textWidth(textX.substring(0, i)) + textWidth(textX[i]) / 2 + i * spacing;
+  let charY = height / 2 + sin((frameCount * speed) + i * waveStretch) * textAmplitude; 
+
+  text(textX[i], charX, charY); 
 }
+}
+	function keyPressed() {
+  if (key = ' ') {
+    textAmplitude = random(20, 100);
+  }
+	
 }
